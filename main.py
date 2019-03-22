50e68b9ff7ed52f46042d4888052e08aa22bb310
GoatDeveloper7 <48812752+GoatDeveloper7@users.noreply.github.com>
2019-03-21 22:28:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ SCREENSIZE = jdata["SCREENSIZE"]
 
 pygame.init()
 pygame.font.init()
-screen = pygame.display.set_mode(SCREENSIZE)
+if jdata["FULLSCREEN"] == "True":
+    screen = pygame.display.set_mode(SCREENSIZE, pygame.FULLSCREEN)
+else:
+    screen = pygame.display.set_mode(SCREENSIZE)
 TEXT = pygame.font.Font('freesansbold.ttf',30)
 
 pygame.display.set_caption('ZenPaint v0.01')
@@ -59,17 +62,76 @@ class canvas(object):
     def __init__(self, sx, sy):
         self.sx = sx
         self.sy = sy
+        self.to_do = []
+        self.actions = []
+        self.temp_store = []
         self.surf = pygame.Surface((sx, sy))
         self.surf.fill((0,0,0))
 
     def update(self, posx, posy, color, brush_size):
+        self.temp_store.append([posx, posy, brush_size])
         for x in range(0, brush_size):
             for y in range(0, brush_size):
                 self.surf.set_at((int(posx-(brush_size/2)+x),int(posy-(brush_size/2)+y)), color)
 
+    def fl(self, x,y,t,r):
+        try:
+            n = self.surf.get_at((x,y))[:3]
+        except Exception:
+            return
+        if t == r:
+            return
+        if n[0] == t[0] and n[1] == t[1] and n[2] == t[2]:
+            pass
+        else:
+            return
+        self.surf.set_at((x,y), r)
+        self.to_do.append((x+1,y,t,r))
+        self.to_do.append((x-1,y,t,r))
+        self.to_do.append((x,y+1,t,r))
+        self.to_do.append((x,y-1,t,r))
+        return
+
+    def flood(self, posx, posy, color_t, color_r):
+        posx = int(posx)
+        posy = int(posy)
+        self.to_do = []
+        self.to_do.append((posx, posy, color_t, color_r))
+        """
+        Flood-fill (node, target-color, replacement-color):
+        1. If target-color is equal to replacement-color, return.
+        2. If the color of node is not equal to target-color, return.
+        3. Set the color of node to replacement-color.
+        4. Perform Flood-fill (one step to the south of node, target-color, replacement-color).
+            Perform Flood-fill (one step to the north of node, target-color, replacement-color).
+            Perform Flood-fill (one step to the west of node, target-color, replacement-color).
+            Perform Flood-fill (one step to the east of node, target-color, replacement-color).
+        5. Return.
+        """
+        
+        while len(self.to_do) > 0:
+            self.fl(self.to_do[0][0],self.to_do[0][1],self.to_do[0][2],self.to_do[0][3])
+            del self.to_do[0]
+            
+
+    def update_no_undo(self, posx, posy, brush_size):
+        for x in range(0, brush_size):
+            for y in range(0, brush_size):
+                self.surf.set_at((int(posx-(brush_size/2)+x),int(posy-(brush_size/2)+y)), (0,0,0))
+
+    def add_temp(self):
+        self.actions.append(self.temp_store)
+        self.temp_store = []
 
     def show(self, surf, cam_rel):
         surf.blit(self.surf, (cam_rel[0], cam_rel[1]))
+
+    def test(self, pos, cam_rel):
+        rect = pygame.Rect(cam_rel[0], cam_rel[1], self.sx, self.sy)
+        if rect.collidepoint(pos[0], pos[1]):
+            return True
+        else:
+            return False
 
 
 im = canvas(jdata["CANVASSIZE"][0], jdata["CANVASSIZE"][1])
@@ -110,6 +172,10 @@ mbposx, mbposy = percentPos(1,92)
 temp = TEXT.render(f"color R:{c_color[0]} G:{c_color[1]} B:{c_color[2]}, Brush Size {b_size}",True,(0,0,0))
 info = pygame.transform.scale(temp, (SCREENSIZE[0]-150, 50))
 
+m_continuos = False
+do_up = False
+s_foc = False
+
 timer = pygame.time.Clock()
 
 running = True
@@ -130,10 +196,17 @@ while running:
     mouse_pos = pygame.mouse.get_pos()
     mouse_left = pygame.mouse.get_pressed()[0]
     mouse_right = pygame.mouse.get_pressed()[2]
+    mouse_middle = pygame.mouse.get_pressed()[1]
+
+    if mouse_left and not m_continuos:
+        m_continuos = True
+
+    if m_continuos and not mouse_left:
+        m_continuos = False
 
     keys = pygame.key.get_pressed()
 
-    #""" Screen panning by moving mouse to edge of screen
+    #""" camera scroll
     if keys[pygame.K_a]:
         cam_pos[0] -= 1
     if keys[pygame.K_d]:
@@ -142,6 +215,17 @@ while running:
         cam_pos[1] -= 1
     if keys[pygame.K_s]:
         cam_pos[1] += 1
+    if keys[pygame.K_z]: #UNDO (note, Is retarted.)s
+        if len(im.actions) > 0 and delay < 0:
+            delay = 20
+            work = im.actions[len(im.actions)-1]
+            im.actions.pop()
+            for t in work:
+                im.update_no_undo(t[0], t[1], t[2])
+    if keys[pygame.K_ESCAPE]:
+        pygame.quit()
+
+    cam_rel = (cam_pos_const[0] - cam_pos[0], cam_pos_const[1] - cam_pos[1])
 
     if SizeUp.test(mouse_pos) and mouse_left and delay < 0:
         b_size += 1
@@ -173,8 +257,6 @@ while running:
         ui = True
         pygame.image.save(im.surf, "MyZenDrawing.png")
 
-    cam_rel = (cam_pos_const[0] - cam_pos[0], cam_pos_const[1] - cam_pos[1])
-
     #print(mouse_pos)
 
     if not ui:
@@ -183,10 +265,19 @@ while running:
 
         if mouse_left:
             im.update(mouse_pos[0]-cam_rel[0], mouse_pos[1]-cam_rel[1], c_color, b_size)
+        
+        if mouse_middle:
+            ap = im.surf.get_at((int(mouse_pos[0]-cam_rel[0]), int(mouse_pos[1]-cam_rel[1])))
+            im.flood(mouse_pos[0]-cam_rel[0], mouse_pos[1]-cam_rel[1], ap[:3], c_color)
 
     screen.fill(b_color)
 
+    s_foc = im.test(mouse_pos, cam_rel)
+
     im.show(screen, cam_rel)
+
+    if not m_continuos and do_up and s_foc:
+        im.add_temp()
 
     if ui:
         delay = 20
@@ -211,4 +302,8 @@ while running:
     screen.blit(info, (mbposx, mbposy))
 
     pygame.display.flip()
+    if not m_continuos or delay > 0:
+        do_up = False
+    else:
+        do_up = True
     
